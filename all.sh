7c3a5991f9fbc8fa75066395da9f8b5efdab2fcd
Frederik Dudzik <frederik@dudzik.co>
2016-02-26 19:29:34 +0100

    fix dev and gui installment

--- all.sh
+++ all.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 bash <(curl -fsSL https://raw.githubusercontent.com/Doodzik/osx_init/master/basic.sh)
-
-./dev.sh
-./gui.sh
+bash <(curl -fsSL https://raw.githubusercontent.com/Doodzik/osx_init/master/dev.sh)
+bash <(curl -fsSL https://raw.githubusercontent.com/Doodzik/osx_init/master/gui.sh)
 
 exit 0
