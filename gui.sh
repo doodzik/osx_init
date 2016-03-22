6f1195935ac035f6a09facf787adbeb76a184cc2
Frederik Dudzik <frederik@dudzik.co>
2016-03-22 15:29:33 +0100

    set safari history to two weeks via applescript

--- gui.sh
+++ gui.sh
@@ -16,10 +16,22 @@ echo 'setup icloud && opening apps'
 echo 'Press [Enter] if done'
 sleep 2
 open -a System\ Preferences
-osascript -e 'tell application "Safari"
+
+osascript -e 'tell application "Safari" to activate
                 open location "https://www.ublock.org"
                 open location "http://web.whatsapp.com"
                 open location "https://github.com/guyht/vimari/releases/latest"
+
+                delay 1
+                tell application "System Events" to tell process "Safari"
+                  keystroke "," using command down
+
+                  tell group 1 of group 1 of window 1
+                      tell pop up button 6
+                        click
+                        click menu item "After two weeks" of menu 1
+                      end tell
+                  end tell
               end tell'
 open -a Flux
 open -a Anki
